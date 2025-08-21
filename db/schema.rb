6ce97d4a1a0018f01a989e1af46e751ccc6bcca2
alexiapierens <alexia.pierens@kedgebs.com>
2025-08-21 14:39:54 +0200

    sign-in front

--- db/schema.rb
+++ db/schema.rb
@@ -9,9 +9,9 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-  ActiveRecord::Schema[7.1].define(version: 2025_08_19_144945) do
-# These are extensions that must be enabled in order to support this database
 
+ActiveRecord::Schema[7.1].define(version: 2025_08_19_144945) do
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "animals", force: :cascade do |t|
