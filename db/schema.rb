91716f91c960a7206011c685ff938f6ec764940e
Alister FAUDEMER <al.faudemer@protonmail.com>
2025-08-22 11:18:57 +0200

    ouais

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.1].define(version: 2025_08_21_130750) do
-=======
-ActiveRecord::Schema[7.1].define(version: 2025_08_20_094445) do
->>>>>>> master
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
